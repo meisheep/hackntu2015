71de55f11691e2c8a483c4fcea6acc841a050f0f
meisheep <immeisheep@gmail.com>
2015-08-22 23:22:43 +0800

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+$(function() {
+    $('.menu .item')
+        .tab()
+    ;
+
+    $('#list .ui.rating')
+        .rating('disable')
+    ;
+
+    $('#review .ui.rating')
+        .rating()
+    ;
+
+    $('#create .ui.rating')
+        .rating()
+    ;
+
+    $('#create .ui.checkbox')
+        .checkbox()
+    ;
+    $('#create .ui.radio.checkbox')
+        .checkbox()
+    ;
+});
